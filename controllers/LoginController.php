5843190eedfc0a3e3ff661afdac738cc3c4fcc3a
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-03-10 11:45:56 +0100

    feat: :art: Creando Rutas y Controlador para Autenticación
    Creando archivo LoginController  y llamandolo desde index.php

---
+++ controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Controllers;
+
+class LoginController {
+    public static function login() {
+        echo "Desde Login";
+    }
+
+    public static function logout() {
+        echo "Desde LogOut";
+    }
+
+    public static function olvide() {
+        echo "Desde Olvide";
+    }
+    
+    public static function recuperar() {
+        echo "Desde Recuperar";
+    }
+}
+?>
