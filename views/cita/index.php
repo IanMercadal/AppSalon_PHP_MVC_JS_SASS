99bab35577871b752b349768a8eb1be8311013cf
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-03-15 15:53:37 +0100

    feat: :art: Mostrando el nombre del usuario autenticado
    Mostrando nombre de user en la página cita

--- views/cita/index.php
+++ views/cita/index.php
@@ -4,15 +4,44 @@
 <div id="app">
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
-        <p>Elige tus servicios a continuación</p>
+        <p class="text-center">Elige tus servicios a continuación</p>
         <div id="servicios" class="listado-servicios"></div>
     </div>
     <div id="paso-2" class="seccion">
         <h2>Tus datos y citas</h2>
-        <p>Coloca tus datos y fecha de tu cita</p>
+        <p class="text-center">Coloca tus datos y fecha de tu cita</p>
+
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input
+                    id="nombre"
+                    type="text"
+                    placeholder="Tu Nombre"
+                    value="<?php echo $nombre; ?>"
+                    disabled
+                />
+            </div>
+
+            <div class="campo">
+                <label for="fecha">Fecha</label>
+                <input
+                    id="fecha"
+                    type="date"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="hora">Hora</label>
+                <input
+                    id="hora"
+                    type="time"
+                />
+            </div>
+        </form>
     </div>
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
-        <p>Verifica que la info sea correcta</p>
+        <p class="text-center">Verifica que la info sea correcta</p>
     </div>
 </div>
