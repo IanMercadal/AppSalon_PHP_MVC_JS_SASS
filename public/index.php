f8037c08b04d5a3a625154a33bf1e25de462fb03
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-03-15 15:40:26 +0100

    feat: :art: Creando Routing y Controllers para las Citas
    Continuando el mvc de citas

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use Controllers\LoginController;
+use Controllers\CitaController;
 use MVC\Router;
 $router = new Router();
 
@@ -24,5 +25,8 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 // Confirmar cuenta
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
+
+// Area privada
+$router->get('/cita', [CitaController::class, 'index']);
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
