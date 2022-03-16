207906775a316c0c2934085064cbd1ff4cdda478
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-03-16 16:43:47 +0100

    feat: :art: Añadiendo o quitando servicios
    Aplicando lógica  y estilos

--- src/js/app.js
+++ src/js/app.js
@@ -144,9 +144,18 @@ function mostrarServicios(servicios) {
 function seleccionarServicio(servicio) {
     const {id} = servicio;
     const {servicios} = cita;
-    cita.servicios = [...servicios, servicio];
 
     const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
-    divServicio.classList.add('seleccionado')
+    // Comprobar si un servicio ya fue agregado
+    if(servicios.some(agregado => agregado.id === id)) {
+        // Eliminar
+        cita.servicios = servicios.filter(agregado => agregado.id !== id);
+        divServicio.classList.remove('seleccionado')
+    } else {
+        // Añadelo
+        cita.servicios = [...servicios, servicio];
+        divServicio.classList.add('seleccionado')
+    }
+    
     console.log(cita)
 }
