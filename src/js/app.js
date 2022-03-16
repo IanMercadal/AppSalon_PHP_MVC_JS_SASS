75144580c3fc889e4c2835cdcb55e664d1343d1c
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-03-16 18:00:39 +0100

    feat: :art: Aplicando estilos al resumen
    Aplicando a cada apartado del resumen

--- src/js/app.js
+++ src/js/app.js
@@ -242,15 +242,12 @@ function mostrarResumen() {
     // Cambiar al div resumen
     const {nombre, fecha, hora, servicios} = cita;
 
-    const nombreCliente = document.createElement('P');
-    nombreCliente.innerHTML = `<span>Nombre:</span> ${nombre}`;
+    // Heading para servicios en Resumen
+    const headingServicios = document.createElement('H3');
+    headingServicios.textContent = 'Resumen de Servicios';
+    resumen.appendChild(headingServicios);
 
-    const fechaCita = document.createElement('P');
-    fechaCita.innerHTML = `<span>Fecha:</span> ${fecha}`;
-
-    const horaCita = document.createElement('P');
-    horaCita.innerHTML = `<span>Hora:</span> ${hora}`;
-
+    // Iterando y mostrando los servicios
     servicios.forEach(servicio => {
         const {id, precio, nombre} = servicio;
         const contenedorServicio = document.createElement('DIV');
@@ -267,6 +264,20 @@ function mostrarResumen() {
 
         resumen.appendChild(contenedorServicio);
     });
+
+    // Heading para Cita en Resumen
+    const headingCita = document.createElement('H3');
+    headingCita.textContent = 'Resumen de Cita';
+    resumen.appendChild(headingCita);
+
+    const nombreCliente = document.createElement('P');
+    nombreCliente.innerHTML = `<span>Nombre:</span> ${nombre}`;
+
+    const fechaCita = document.createElement('P');
+    fechaCita.innerHTML = `<span>Fecha:</span> ${fecha}`;
+
+    const horaCita = document.createElement('P');
+    horaCita.innerHTML = `<span>Hora:</span> ${hora}`;
 
     resumen.appendChild(nombreCliente);
     resumen.appendChild(fechaCita);
